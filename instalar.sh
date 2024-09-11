8f505440d1f548629ccbe3fc747ea047df0bf7d3
William Santos <thespation@gmail.com>
2024-09-11 14:25:56 -0300

    instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,146 +1,124 @@
 #!/usr/bin/env bash
-# Desenvolvido pelo William Santos
+# Desenvolvido por William Santos
 # contato: thespation@gmail.com ou https://github.com/thespation
 
-# Cores (tabela de cores: https://gist.github.com/avelino/3188137)
-CIAN="\033[0;36m"	#Deixa a saída na cor ciano     
-NORM="\033[0m"		#Volta para a cor padrão        
-VERD="\033[0;32m"	#Deixa a saída na cor verde
-VERM="\033[1;31m"	#Deixa a saída na cor vermelho
+# Cores
+CIAN="\033[0;36m"
+NORM="\033[0m"
+VERD="\033[0;32m"
+VERM="\033[1;31m"
 
-## Alias
-SI="sudo apt install -y"		    #Comando para instalar novos pacotes
-SA="sudo apt"				        #Usado para atualizar sistema
-data_atual=$(date +"%Y%m%d%H%M%S")	#Define a data e hora atual
-GITH="https://raw.githubusercontent.com/thespation/dpux_bspwm/main/scripts/" #Endereço do script (tema e ícone)
-data_atual=$(date +"%Y%m%d%H%M%S")	#Define a data e hora 
-GG="git clone"				        #Comando para baixar arquivos git
-i3pf="$HOME/.config"		        #Endereço das configurações
-i3t="/tmp/i3wm"				        #Pasta temporária
+# Alias
+SI="sudo apt install -y"
+SA="sudo apt"
+GG="git clone"
+GITH="https://raw.githubusercontent.com/thespation/dpux_bspwm/main/scripts/"
+i3pf="$HOME/.config"
+i3t="/tmp/i3wm"
+data_atual=$(date +"%Y%m%d%H%M%S")
 
-#--Função: Atualizar espelhos--#
-declare -f _atualizar.sistema
-function _atualizar.sistema(){
-        echo -e "${CIAN}[ ] Atualizar sistema${NORM}"
-            ${SA} update && ${SA} upgrade -y && ${SA} dist-upgrade -y && #Atualiza sistema
-        echo -e "${VERD}[*] Sistema atualizado${NORM}"
-        _instalar.programas #Chama a função     
+# Listas de pacotes e URLs
+declare -a PACOTES=(
+    i3 xorg i3status lightdm i3lock xsettingsd xfce4-power-manager network-manager sudo
+    dmenu rofi alacritty nm-tray nitrogen feh lxappearance picom thunar tumbler
+    thunar-archive-plugin thunar-volman dh-autoreconf make maim python3-pip git curl
+    python3-i3ipc xdg-user-dirs htop neofetch viewnior cargo xclip yad catfish baobab
+    meld xarchiver geany alsa-utils pulseaudio pacucontrol pulsemixer gcc make libx11-dev
+    libxtst-dev pkg-config sysstat ranger vim hsetroot sysvinit-utils psmisc ncal
+)
+
+# Função: Atualizar sistema
+atualizar_sistema() {
+    echo -e "${CIAN}[ ] Atualizar sistema${NORM}"
+    ${SA} update && ${SA} upgrade -y && ${SA} dist-upgrade -y
+    echo -e "${VERD}[*] Sistema atualizado${NORM}"
+    instalar_programas
 }
 
-#--Função: Instalar programas--#
-declare -f _instalar.programas
-function _instalar.programas(){
-        echo -e "\n${CIAN}[ ] Instalar os programas necessários${NORM}"
-            ${SI} \
-                i3 xorg i3status lightdm i3lock \
-                xsettingsd xfce4-power-manager network-manager sudo \
-                dmenu rofi alacritty nm-tray \
-                nitrogen feh lxappearance picom \
-                thunar tumbler thunar-archive-plugin thunar-volman \
-                dh-autoreconf make maim python3-pip git curl \
-                xdg-user-dirs htop neofetch viewnior cargo xclip \
-                catfish baobab meld xarchiver geany \
-                gcc make libx11-dev libxtst-dev pkg-config sysstat \
-                ranger vim hsetroot sysvinit-utils psmisc ncal
-            xdg-user-dirs-update #Atualiza as pastas de usuário
-            #Compilar i3blocks
-            if [[ ! -d "/tmp/i3blocks" ]]; then # Verifica existencia da pasta /tmp/i3blocks
-                #apt cache search dh-autoreconf
-                cd /tmp && ${GG} https://github.com/vivien/i3blocks
-                cd i3blocks; chmod +x autogen.sh
-                ./autogen.sh; ./configure
-                make; sudo make install
-            fi
-            echo -e "${VERD}[*] Aplicativos necessários instalados${NORM}"
-            curl -s ${GITH}temas.sh | bash
-            curl -s ${GITH}icones.sh | bash
-         _ksuperkey #Chama a função
-}
+# Função: Instalar programas
+instalar_programas() {
+    echo -e "\n${CIAN}[ ] Instalando programas${NORM}"
+    ${SI} "${PACOTES[@]}"
+    xdg-user-dirs-update
 
-#--Função: Adicionar função de abrir menu (rofi) com tecla super (Ksuperkey)--#
-declare -f _ksuperkey
-function _ksuperkey(){
-        if [[ ! -d "/tmp/ksuperkey" ]]; then # Verifica existencia da pasta /tmp/ksuperkey
-            echo -e "\n${CIAN}[ ] Habilitar tecla Super para abrir menu" ${NORM}
-                cd /tmp && ${GG} https://github.com/hanschen/ksuperkey.git &&
-                cd ksuperkey
-                make && sudo make install
-	fi
-        echo -e "${VERD}[*] Tecla Super habilitada com sucesso" ${NORM}
-        _lightdm #Chama a função  
-}
-
-#--Função: Deixar usuário visível no login do LightDM--#
-declare -f _lightdm
-function _lightdm(){
-    #Alias locais
-    lightdm="/etc/lightdm/lightdm.conf" #Local do arquivo original
-    ldm="/etc/lightdm/lightdm-gtk-greeter.conf"	#Caminho de configurção do lightDM
-            
-    if [[ -f "${lightdm}" ]]; then #Verifica a existência do arquivo de configuração
-        echo -e "\n${CIAN}[ ] Criando backup do arquivo 'lightdm.conf"${NORM}
-            sudo cp ${lightdm} ${lightdm}_BKP_${data_atual} &&
-        echo -e "${VERD}[*] Backup criado com sucesso"
-            sudo sed -i 's/^#greeter-hide-users=false/greeter-hide-users=false/g' ${lightdm} #Descomenta a linha
+    if [[ ! -d "/tmp/i3blocks" ]]; then
+        cd /tmp && ${GG} https://github.com/vivien/i3blocks
+        cd i3blocks && chmod +x autogen.sh && ./autogen.sh && ./configure && make && sudo make install
     fi
-    #Personalização da tela de login - lightdm
-	if [[ -f ${ldm} ]]; then # Verifica existencia o arquivo
-		mv ${ldm} ${ldm}_BKP_${data_atual}
-	sudo cp -rf  ${i3t}/config/lightdm-gtk-greeter.conf ${ldm}
-    echo -e "${VERD}[*] lightdm configurado"${NORM}
-	fi  
-    _xinit #Chama a função
+
+    echo -e "${VERD}[*] Aplicativos instalados${NORM}"
+    curl -s ${GITH}temas.sh | bash
+    curl -s ${GITH}icones.sh | bash
+    ksuperkey
 }
 
-#--Função: Habilitar inicialização i3 no arquivo ~/.xinitrc --#
-declare -f _xinit
-    function _xinit(){
-        echo -e "\n${CIAN}[ ] Habilitar inicialização do i3${NORM}"
-        if [[ ! -f ~/.xinitrc ]]; then #Verifica a existência do arquivo de configuração
-            cp /etc/X11/xinit/xinitrc ~/.xinitrc
-        fi
-        sed -i 's/^exec xterm -geometry 80x66+0+0 -name login/#exec xterm -geometry 80x66+0+0 -name login/' ~/.xinitrc
-            echo "exec i3" >> ~/.xinitrc
-        echo -e "${VERD}[*] i3 habilitado${NORM}"
-        _personalizacao #Chama a função
+# Função: Habilitar tecla Super para abrir menu
+ksuperkey() {
+    if [[ ! -d "/tmp/ksuperkey" ]]; then
+        echo -e "\n${CIAN}[ ] Habilitar tecla Super${NORM}"
+        cd /tmp && ${GG} https://github.com/hanschen/ksuperkey.git
+        cd ksuperkey && make && sudo make install
+    fi
+    echo -e "${VERD}[*] Tecla Super habilitada${NORM}"
+    lightdm
 }
 
-#--Função: Copiar personalizações--#
-declare -f _personalizacao
-function _personalizacao(){
-	#Baixa as novas configurações para a pasta "/tmp/i3wm"
-	if [[ ! -d "${i3t}" ]]; then # Verifica existencia da pasta /tmp/i3wm
-		echo -e "${CIAN}[ ] Baixando configurações personalizadas ${NORM}"
-		cd  /tmp && ${GG} https://github.com/thespation/i3wm
-        fi
-	#Caso tenha a pasta "~/.config/i3" vai fazer bkp antes de copiar novas configurações
-	if [[ -d "${i3pf}" ]]; then # Verifica existencia da pasta i3 no perfil do usuário
-		mv ${i3pf}/i3 ${i3pf}/i3_BKP_${data_atual}
-		echo -e "${VERD}[*] Foi realizado um bkp da pasta \"~/.config/i3\" ${NORM}"
-	fi
-		echo -e "${CIAN}[ ] Copiar configurações para pastas corretas ${NORM}"
-		mkdir -p ${i3pf}/i3 && cp -rf ${i3t}/i3/* ${i3pf}/i3 && chmod +x ${i3pf}/i3/* -R
-		cp -rf ${i3t}/fonts $HOME/.local/share 
-	#Cria bkp do arquivo "$HOME/.gtkrc-2.0" antes de copiar o novo
-	if [[ -f $HOME/.gtkrc-2.0 ]]; then # Verifica existencia o arquivo
-		mv $HOME/.gtkrc-2.0 $HOME/.gtkrc-2.0_BKP_${data_atual}
-	fi	
-		cp -rf ${i3t}/config/.gtkrc-2.0 $HOME/.gtkrc-2.0
-	#Cria bkp do arquivo "~/.config/gtk-3.0/settings.ini" antes de copiar o novo
-	if [[ -f ${i3pf}/gtk-3.0/settings.ini ]]; then # Verifica existencia o arquivo
-		mv ${i3pf}/gtk-3.0/settings.ini ${i3pf}/gtk-3.0/settings.ini_BKP_${data_atual}
-	fi	
-		mkdir -p ${i3pf}/gtk-3.0 && cp -rf ${i3t}/config/settings.ini ${i3pf}/gtk-3.0/settings.ini
-        echo -e "${VERD}[*] Configurações copiadas ${NORM}"
+# Função: Configurar LightDM
+lightdm() {
+    local lightdm_conf="/etc/lightdm/lightdm.conf"
+    local ldm_conf="/etc/lightdm/lightdm-gtk-greeter.conf"
+
+    [[ -f "${lightdm_conf}" ]] && {
+        echo -e "\n${CIAN}[ ] Criando backup de lightdm.conf${NORM}"
+        sudo cp ${lightdm_conf} ${lightdm_conf}_BKP_${data_atual}
+        sudo sed -i 's/^#greeter-hide-users=false/greeter-hide-users=false/' ${lightdm_conf}
+    }
+
+    [[ -f ${ldm_conf} ]] && {
+        mv ${ldm_conf} ${ldm_conf}_BKP_${data_atual}
+        sudo cp -rf ${i3t}/config/lightdm-gtk-greeter.conf ${ldm_conf}
+        sudo cp -rf ${i3t}/i3/wallpapers/mono.png /usr/share/images/desktop-base/wallpaper.jpg
+        echo -e "${VERD}[*] lightdm configurado${NORM}"
+    }
+
+    xinit
 }
 
-#Vefifica se está usando Debian ou derivado
-	clear
-	if [[ -f "/etc/debian_version" ]]; then #Verifica a existencia do arquivo "debian_version" 
-		echo -e "${CIAN}[i] Script ${VERM}PESSOAL${CIAN} para instalação do i3wm no Debian 12"
-		echo -e "\n${CIAN}[!] Instalação começará em ${VERM}10 segundos${CIAN}, para cancelar pressione: \"${VERM}Ctrl+c\"${NORM}"
-		sleep 10
-		_atualizar.sistema #Chama a função
-	else
-		echo -e "${VERM}[!] Esse script PESSOAL foi desenvolvido para rodar no Debian e seus derivados.${NORM}"        
+# Função: Habilitar inicialização do i3
+xinit() {
+    echo -e "\n${CIAN}[ ] Habilitar inicialização do i3${NORM}"
+    [[ ! -f ~/.xinitrc ]] && cp /etc/X11/xinit/xinitrc ~/.xinitrc
+    sed -i 's/^exec xterm -geometry 80x66+0+0 -name login/#exec xterm -geometry 80x66+0+0 -name login/' ~/.xinitrc
+    echo "exec i3" >> ~/.xinitrc
+    echo -e "${VERD}[*] i3 habilitado${NORM}"
+    personalizacao
+}
+
+# Função: Copiar personalizações
+personalizacao() {
+    [[ ! -d "${i3t}" ]] && cd /tmp && ${GG} https://github.com/thespation/i3wm
+
+    [[ -d "${i3pf}/i3" ]] && mv ${i3pf}/i3 ${i3pf}/i3_BKP_${data_atual}
+    mkdir -p ${i3pf}/i3 && cp -rf ${i3t}/i3/* ${i3pf}/i3 && chmod +x ${i3pf}/i3/* -R
+    cp -rf ${i3t}/fonts $HOME/.local/share 
+
+    [[ -f $HOME/.gtkrc-2.0 ]] && mv $HOME/.gtkrc-2.0 $HOME/.gtkrc-2.0_BKP_${data_atual}
+    cp -rf ${i3t}/config/.gtkrc-2.0 $HOME/.gtkrc-2.0
+
+    mkdir -p ${i3pf}/gtk-3.0
+    [[ -f ${i3pf}/gtk-3.0/settings.ini ]] && mv ${i3pf}/gtk-3.0/settings.ini ${i3pf}/gtk-3.0/settings.ini_BKP_${data_atual}
+    cp -rf ${i3t}/config/settings.ini ${i3pf}/gtk-3.0/settings.ini
+
+    echo -e "${VERD}[*] Configurações copiadas${NORM}"
+}
+
+# Verifica se está usando Debian ou derivado
+clear
+if [[ -f "/etc/debian_version" ]]; then
+    echo -e "${CIAN}[i] Script ${VERM}PESSOAL${CIAN} para instalação do i3wm no Debian 12"
+    echo -e "\n${CIAN}[!] Instalação começará em ${VERM}10 segundos${CIAN}, para cancelar pressione: \"${VERM}Ctrl+c\"${NORM}"
+    sleep 10
+    atualizar_sistema
+else
+    echo -e "${VERM}[!] Esse script PESSOAL foi desenvolvido para rodar no Debian e seus derivados.${NORM}"
 fi
